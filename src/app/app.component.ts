7294301e67caa148fab4523aa763883d7ef2bb00
MarcaCriss <54193589+MarcaCriss@users.noreply.github.com>
2021-11-12 06:55:57 -0400

    lazy load with character

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { RickService } from './services/rick.service';
-import { Character } from './interfaces/rick.interface';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Personajes de la serie "Rick And Morty"';
-  character$!: Observable<Character>;
-  characters$!: Observable<Character[]>;
-  next = 1;
-  search = '';
-
-  constructor(private rick: RickService ) {}
-
-  ngOnInit() {
-    this.filterCharacter();
-  }
-
-  pagesPlus() {
-    this.next++;
-    this.filterCharacter();
-  }
-
-  pagesMinus() {
-    if (this.next > 1) {
-      this.next--;
-      this.filterCharacter();
-    }
-  }
-
-  get pages() {
-    return this.rick.pages;
-  }
-
-  getCharacter(id: number) {
-    this.character$ = this.rick.getCharacter(id);
-  }
-
-  filterCharacter() {
-    this.characters$ = this.rick.filterCharacter(this.search, this.next);
-  }
-
-  changeValueSearch() {
-    this.next = 1;
-    this.characters$ = this.rick.filterCharacter(this.search, this.next);
-  }
 
 }
